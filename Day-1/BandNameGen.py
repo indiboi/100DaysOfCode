ecfec38c8d1b89924ae94a9a3f6e5b1217c9fed8
Hriday Nichani <hriday.nichani@gmail.com>
2024-05-23 09:46:26 +0530

    Completed Day 1 of 100days of code

---
+++ Day-1/BandNameGen.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!")
+x = str(input("Enter the name of the city you were born in:"))
+y = str(input("Enter the name of your pet:"))
+z = x + " " + y 
+print(f"Your band name is {z}")
