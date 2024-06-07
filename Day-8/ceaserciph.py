74b634f95d8586369fe3e298488cfa91f07fb95e
Hriday Nichani <hriday.nichani@gmail.com>
2024-06-07 12:06:10 +0530

    Completed Day-8 :)

---
+++ Day-8/ceaserciph.py
@@ -0,0 +1,50 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+user_answer = True
+
+def encrypt():
+    cipher = ''
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position + shift
+        new_alphabet = alphabet[new_position]
+        cipher += new_alphabet
+    print(cipher)
+
+def decrypt():
+    cipher = ''
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position - shift
+        new_alphabet = alphabet[new_position]
+        cipher += new_alphabet
+    print(cipher)
+
+
+def ceasercipher(direction, text, shift):
+    if direction == "decode":
+        shift *= -1
+    cipher = ""
+    for char in text: 
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift
+            new_alphabet = alphabet[new_position]
+            cipher += new_alphabet
+        else:
+            cipher += char
+    print(cipher)
+
+while user_answer != False:
+    direction = input("Enter 'encode' to encrypt and 'decode' to decrypt: ").lower()
+    text = input("Enter the text: ")
+    shift = int(input("Enter the shift: "))
+    if shift >= 26:
+        shift = shift%26
+    ceasercipher(direction, text, shift)
+    continue_or_not = input("Do you want to continue?").lower()
+    if continue_or_not == "yes":
+        user_answer = True
+    if continue_or_not == "no":
+        user_answer = False
+
+
